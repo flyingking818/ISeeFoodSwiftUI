3cf56a095f6d30cd4a9c96c3571802215bce37bb
flyingking888 <jeremyfeiwang@outlook.com>
2026-04-22 11:08:08 -0400

    Initial Commit

---
+++ ISeeFoodSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ISeeFoodSwiftUI
+//
+//  Created by Jeremy Wang on 4/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ISeeFoodSwiftUI/ISeeFoodSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISeeFoodSwiftUIApp.swift
+//  ISeeFoodSwiftUI
+//
+//  Created by Jeremy Wang on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct ISeeFoodSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
